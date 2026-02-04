74ed266daa7d426b14b3a24eb8852a17240b7bef
Eyaaessid <eyae2684@gmail.com>
2026-02-04 13:52:56 +0100

    Add admin dashboard controller + routes for all admin pages

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin', name: 'admin_')]
+class AdminController extends AbstractController
+{
+    // Dashboard / Home
+    #[Route('', name: '', methods: ['GET'])]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+
+    // Tables page
+    #[Route('/tables', name: 'tables', methods: ['GET'])]
+    public function tables(): Response
+    {
+        return $this->render('admin/table.html.twig');
+    }
+
+    // Typography page
+    #[Route('/typography', name: 'typography', methods: ['GET'])]
+    public function typography(): Response
+    {
+        return $this->render('admin/typography.html.twig');
+    }
+
+    // Widgets page
+    #[Route('/widgets', name: 'widgets', methods: ['GET'])]
+    public function widgets(): Response
+    {
+        return $this->render('admin/widget.html.twig');
+    }
+
+    // Forms page
+    #[Route('/forms', name: 'forms', methods: ['GET'])]
+    public function forms(): Response
+    {
+        return $this->render('admin/form.html.twig');
+    }
+
+    // Elements → Buttons
+    #[Route('/elements/buttons', name: 'elements_buttons', methods: ['GET'])]
+    public function buttons(): Response
+    {
+        return $this->render('admin/button.html.twig');
+    }
+
+    // Elements → Other Elements
+    #[Route('/elements/other', name: 'elements_other', methods: ['GET'])]
+    public function otherElements(): Response
+    {
+        return $this->render('admin/element.html.twig');
+    }
+
+    // Charts page
+    #[Route('/charts', name: 'charts', methods: ['GET'])]
+    public function charts(): Response
+    {
+        return $this->render('admin/chart.html.twig');
+    }
+
+    // Blank page
+    #[Route('/blank', name: 'blank', methods: ['GET'])]
+    public function blank(): Response
+    {
+        return $this->render('admin/blank.html.twig');
+    }
+
+    // 404 page (useful for testing)
+    #[Route('/404', name: '404', methods: ['GET'])]
+    public function notFound(): Response
+    {
+        return $this->render('admin/404.html.twig');
+    }
+
+    // Sign In page (public-like, but inside admin folder for consistency)
+    #[Route('/login', name: 'login', methods: ['GET'])]
+    public function login(): Response
+    {
+        return $this->render('admin/signin.html.twig');
+    }
+
+    // Sign Up page
+    #[Route('/register', name: 'register', methods: ['GET'])]
+    public function register(): Response
+    {
+        return $this->render('admin/signup.html.twig');
+    }
+}
