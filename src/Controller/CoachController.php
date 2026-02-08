76242dd74dd943c15fba258ceb66e7c887d424ce
hiba <hiba.bhm101@gmail.com>
2026-02-08 02:49:39 +0100

    feat: logout button

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -14,6 +14,7 @@ class CoachController extends AbstractController
     #[Route('', name: 'dashboard')]
     public function dashboard(): Response
     {
+        // Coach uses the frontoffice base template (same as regular users for now)
         return $this->render('base.html.twig', [
             'user' => $this->getUser(),
         ]);
